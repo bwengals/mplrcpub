a4b480a8af6cfe89dac5ae973c73bb05dcbad1d7
Bill Engels <w.j.engels@gmail.com>
2018-07-28 19:33:11 -0500

    changed to use a context manager

--- pubfig_settings.py
+++ pubfig_settings.py
@@ -1,77 +1,107 @@
 #/usr/bin/env python
 
+import matplotlib
 import matplotlib.pyplot as plt
 import os
 import random
-from matplotlib import rc
-
-rc('text', usetex=True)
-rc('mathtext', default='sf')
-
-rc('font', size=10)
-rc('font', family='serif')
-rc('font', serif='palatino')
-rc('font', weight='medium')
-
-rc("lines", markeredgewidth=1)
-rc("lines", linewidth=1)
-
-rc("axes", labelsize=10)
-rc("axes", linewidth=0.8)
-rc("axes", titlesize='medium')
-rc("axes", unicode_minus=False)
-
-rc('xtick',       labelsize=10.0)
-rc('xtick.major', pad=4.0)
-rc('xtick.major', size=4.0)
-rc('xtick.major', width=0.8)
-rc('xtick.minor', pad=2.0)
-rc('xtick.minor', size=2.0)
-rc('xtick.minor', width=0.8)
-
-rc('ytick',       labelsize=10.0)
-rc('ytick.major', pad=4.0)
-rc('ytick.major', size=4.0)
-rc('ytick.major', width=0.8)
-rc('ytick.minor', pad=2.0)
-rc('ytick.minor', size=2.0)
-rc('ytick.minor', width=0.8)
-
-rc('text.latex', unicode=True)
-rc('savefig', dpi=300.0)
-rc('legend', fontsize=10)
-rc('legend', scatterpoints=1)
-rc('legend', frameon=False)
-rc('image', cmap='afmhot')
-rc('image', interpolation='none')
-rc('figure', frameon=False)
-rc('figure', dpi=300.0)
-rc('pdf', compression=6)
+import time
 
 
 green1='#00ff00'
 orange='#ffba00'
+onecolumn_width = 3.6
+twocolumn_width = 7.0
 
-def make_figure(width="onecolumn", height=2):
-    assert width is "onecolumn" or "twocolumn", "Must be 'onecolumn' or 'twocolumn"
-    global NUMCOLUMNS
-    if width is "onecolumn":
-        w = 3.6
-        NUMCOLUMNS = 1
-    else:
-        w = 7.0
-        NUMCOLUMNS = 2
-    fig = plt.figure(figsize=(w, height), frameon=False)
-    return fig
 
-def preview_in_latex():
-    if NUMCOLUMNS == 1:
-        plt.savefig("/tmp/onecolumn.pdf")
-    elif NUMCOLUMNS == 2:
-        plt.savefig("/tmp/twocolumn.pdf")
-    else:
-        raise TypeError("unknown number of columns")
-    doc = r"""
+class FigConfig(object):
+    def __init__(self, num_columns=1, height=3,
+                       top=0.88, bottom=0.11, left=0.125, right=0.9,
+                       hspace=0.2, wspace=0.2,
+                       fontsize=10):
+        if num_columns == 1:
+            self.width = onecolumn_width
+        elif num_columns == 2:
+            self.width = twocolumn_width
+        else:
+            raise ValueError("num_columns must be 1 or 2")
+        self.num_columns  = num_columns
+        self.height   = height
+        self.top      = top
+        self.bottom   = bottom
+        self.left     = left
+        self.right    = right
+        self.hspace   = hspace
+        self.wspace   = wspace
+        self.fontsize = fontsize
+
+    def __enter__(self):
+        matplotlib.rcParams.update(
+          {"text.usetex":           True,
+           "mathtext.default":      "sf",
+           "font.size":             self.fontsize,
+           "font.serif":            "Palatino",
+           "font.weight":           "medium",
+           "lines.markeredgewidth": 1,
+           "lines.linewidth":       1,
+           "axes.labelsize":        self.fontsize,
+           "axes.linewidth":        0.8,
+           "axes.titlesize":        "medium",
+           "axes.unicode_minus":    False,
+           "xtick.labelsize":       self.fontsize,
+           "xtick.major.pad":       4.0,
+           "xtick.major.size":      4.0,
+           "xtick.major.width":     0.8,
+           "xtick.minor.pad":       2.0,
+           "xtick.minor.size":      2.0,
+           "xtick.minor.width":     0.8,
+           "ytick.labelsize":       self.fontsize,
+           "ytick.major.pad":       4.0,
+           "ytick.major.size":      4.0,
+           "ytick.major.width":     0.8,
+           "ytick.minor.pad":       2.0,
+           "ytick.minor.size":      2.0,
+           "ytick.minor.width":     0.8,
+           "text.latex.unicode":    True,
+           "pgf.texsystem":         "pdflatex",
+           "savefig.dpi":           300,
+           "savefig.pad_inches":    0.01,
+           "savefig.format":        "pdf",
+           "legend.fontsize":       self.fontsize,
+           "legend.scatterpoints":  1,
+           "legend.frameon":        False,
+           "image.cmap":            "inferno",
+           "image.interpolation":   "none",
+           "image.origin":          "lower",
+           "figure.frameon":        False,
+           "figure.dpi":            300,
+           "figure.subplot.top":    self.top,
+           "figure.subplot.bottom": self.bottom,
+           "figure.subplot.left":   self.left,
+           "figure.subplot.right":  self.right,
+           "figure.subplot.hspace": self.hspace,
+           "figure.subplot.wspace": self.wspace,
+           "pdf.compression":       6})
+        return plt.figure(figsize=(self.width, self.height))
+
+    def __exit__(self, *args):
+        print("ARGS:", args)
+        if self.num_columns == 1:
+            plt.savefig(r"/tmp/onecolumn.pdf")
+            plt.savefig(r"/tmp/twocolumn.pdf")
+            # make test figure to save as two column
+            #fig = plt.figure(figsize=(self.twocolumn_width, 3))
+            #ax = fig.gca()
+            #plt.hist(np.random.randn(500), 40)
+            #plt.savefig("/tmp/twocolumn.pdf")
+        else:
+            plt.savefig(r"/tmp/onecolumn.pdf")
+            plt.savefig(r"/tmp/twocolumn.pdf")
+            # make test figure to save as one column
+            #fig = plt.figure(figsize=(self.onecolumn_width, 3))
+            #ax = fig.gca()
+            #ax.hist(np.random.randn(100), 20)
+            #plt.savefig("/tmp/onecolumn.pdf")
+        doc = r"""
 %\documentclass[prd, twocolumn, amsmath, floatfix, preprintnumbers, nofootinbib,superscriptaddress,letterpaper]{revtex4-1}
 \documentclass[11pt,twocolumn]{article}
 
@@ -121,17 +151,15 @@ def preview_in_latex():
   \centerline{\includegraphics[width=17.2cm]{/tmp/twocolumn.pdf}}
 \end{figure*}
 
-\lipsum[5-25]
+\lipsum[5-10]
 
 \end{document}
 """
-    with open("/tmp/ldoc.tex", 'wb') as f:
-        f.write(doc)
-
-    os.system("pdflatex -output-directory /tmp /tmp/ldoc.tex")
-    os.system("rm *.log && rm *.bib && rm *.aux")
-    os.system("evince /tmp/ldoc.pdf &")
-
+        matplotlib.rcParams.update(matplotlib.rcParamsDefault)
+        with open("/tmp/ldoc.tex", 'w') as f:
+            f.write(doc)
+        os.system(r"pdflatex -output-directory /tmp /tmp/ldoc.tex")
+        os.system(r"evince /tmp/ldoc.pdf && rm *.log && rm *.bib && rm *.aux")
 
 
 
